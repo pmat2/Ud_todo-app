ad1d51b2d51f0850df170a0bf85644a5ee544d34
Pawel <pawel.matusiak29@gmail.com>
2023-01-24 01:32:06 +0100

    Task Group Controller

---
+++ src/main/java/io/github/pmat/todoapp/controller/TaskGroupController.java
@@ -0,0 +1,57 @@
+package io.github.pmat.todoapp.controller;
+
+import io.github.pmat.todoapp.model.Task;
+import io.github.pmat.todoapp.model.projection.GroupReadModel;
+import io.github.pmat.todoapp.model.projection.GroupWriteModel;
+import io.github.pmat.todoapp.repository.TaskRepository;
+import io.github.pmat.todoapp.service.TaskGroupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/group")
+public class TaskGroupController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
+
+    private TaskGroupService service;
+    private TaskRepository repository;
+
+    public TaskGroupController(TaskGroupService taskGroupService, TaskRepository repository) {
+        this.service = taskGroupService;
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GroupReadModel>> getTaskGroups(){
+        logger.info("[getTaskGroups] - returning all groups");
+        return ResponseEntity.ok(service.readAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel source){
+        logger.info("[createGroup] - create group for: {}", source);
+        return ResponseEntity.created(URI.create("/")).body(service.createGroup(source));
+    }
+
+    @Transactional
+    @PatchMapping("/toggle/{id}")
+    public ResponseEntity<?> toggleGroup(@PathVariable Integer id){
+        logger.info("[toggleGroup] - toggle group with id: {}", id);
+        service.toggleGroup(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/tasks")
+    public ResponseEntity<List<Task>> getTasksFromGroup(@PathVariable Integer id){
+        logger.info("[getTasksFromGroup] - for group id: {}", id);
+        return ResponseEntity.ok(repository.findByGroupId(id));
+    }
+}
