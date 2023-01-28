7738c1581f2ba20eca1ea63f2aabb48852b42e56
Pawel <pawel.matusiak29@gmail.com>
2023-01-29 00:17:51 +0100

    Refresh projects, index, tasks

--- src/main/java/io/github/pmat/todoapp/controller/ProjectController.java
+++ src/main/java/io/github/pmat/todoapp/controller/ProjectController.java
@@ -53,6 +53,7 @@ public class ProjectController {
         }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
         model.addAttribute("message", "Project added");
         return "projects";
     }
